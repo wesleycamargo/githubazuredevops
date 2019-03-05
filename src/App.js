963da1d62dff4b5cd15e0427ef390c6c33677bcc
Wesley Camargo <camargo.wes@gmail.com>
2019-03-05 20:34:28 -0300

    ajustes no yaml

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Hello Deliverers!!
+            {/* Edit <code>src/App.js</code> and save to reload. */}
           </p>
           <a
             className="App-link"
